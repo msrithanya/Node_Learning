0c2b1827c8995a443a0b0c5bfb64d790d4d149ad
msrithanya <rithanyamanoharan100@gmail.com>
2025-07-07 18:11:38 -0700

    learning

--- Express_Learning/script.js
+++ Express_Learning/script.js
@@ -10,6 +10,15 @@ app.get("/about",(req,res)=>{
 app.get("/contact",(req,res)=>{
     res.send("this is contact page")
 })
+app.post("/posting",(req,res)=>{
+    res.sendStatus(200)
+})
+app.put("/puting",(req,res)=>{
+    res.sendStatus(202)
+})
+app.patch("/patching",(req,res)=>{
+    res.sendStatus(203)
+})
 app.listen(port,function(){
     console.log(`Rithu server is running ${port}`);
 })
