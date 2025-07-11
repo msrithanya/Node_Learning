b287bc1736d22bb3ec4f398f33ec8a76feccafb1
msrithanya <rithanyamanoharan100@gmail.com>
2025-07-10 18:01:07 -0700

    Secret_project

--- Secret_project/script.js
+++ Secret_project/script.js
@@ -1,16 +1,25 @@
+import bodyParser from "body-parser";
 import express from "express"
-import bodyParser, { urlencoded } from "body-parser";
-import {dirname} from "path";
-import { fileURLToPath } from "url";
-const __dirname =dirname(fileURLToPath(import.meta.url))
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __dirname=dirname(fileURLToPath(import.meta.url))
 const app=express();
-const port=3000;
-app.bodyParser(urlencoded({extended:true}))
- 
-app.get('/',(req,res)=>{
+const port=4000;
+app.use(bodyParser.urlencoded({extended:true}))
+app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html")
 })
-app.post('/submit')
-app.listen(port,(req,res)=>{
-    console.log("listening")
+app.listen(port,()=>{
+    console.log(`${port} server is running`);
 })
+app.post("/login",(req,res)=>{
+    if(req.body.password=="Rithu"){
+         res.sendFile(__dirname+"/`secret`.html")
+        console.log("heyyy") 
+    }
+}
+)
+
+
+
---
+++ squarefile/script.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 5000;
+let uname;
+let pass;
+app.use(bodyParser.urlencoded({ extended: true }));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/users.html");
+});
+
+app.listen(port, () => {
+  console.log(`${port} server is listening:)`);
+});
+app.post("/signin", (req, res) => {
+  uname = req.body.username;
+  pass = req.body.password;
+  res.sendFile(__dirname + "/signin.html");
+});
+
+app.post("/signup", (req, res) => {
+  console.log(uname, pass);
+  if (req.body.susername == uname && req.body.spassword == pass) {
+    res.sendFile(__dirname + "/success.html");
+  } else {
+    res.send(`<h1>Try again venna maganeaaa</h1>`);
+  }
+});
